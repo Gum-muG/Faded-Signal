ee076736300fed4cfee034d2687df8b0efb51449
Gum-muG <monty316.mm@gmail.com>
2026-08-06 03:29:53 -0500

    deleted 7 lines of code(they commited warcrimes)

--- Faded Signal/Assets/Scripts/Camera/MoveCamera.cs
+++ Faded Signal/Assets/Scripts/Camera/MoveCamera.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     [SerializeField] Transform cameraPosition;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.position = cameraPosition.position;
